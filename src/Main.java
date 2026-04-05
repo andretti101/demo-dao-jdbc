1ee1d9cd8de483a2b87ac47c7f22f5f0e95a5a4b
andretti101 <enzoandretti3@gmail.com>
2026-04-05 18:54:40 -0300

    implementação da classe Seller

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department dp =  new Department(1, "Tools");
-        System.out.println(dp);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+
+        System.out.println(seller);
     }
 }
