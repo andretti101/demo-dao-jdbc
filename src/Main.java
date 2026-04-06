1107cc19a6f364ec05489382b927731dfc9ad832
andretti101 <enzoandretti3@gmail.com>
2026-04-05 21:48:59 -0300

    implementação findByDepartment

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import entities.Department;
-import entities.Seller;
-
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
-        Department dp =  new Department(1, "Tools");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
 
-        System.out.println(seller);
     }
 }
